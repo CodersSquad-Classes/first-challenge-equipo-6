f9d30df525b1a8bcc269ef41e9b7ade3b6db33eb
Upland1 <107065818+Upland1@users.noreply.github.com>
2025-02-15 18:23:07 -0600

    Código para ordenar archivo txt

---
+++ Codigo_ordenar.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>  
+#include <algorithm>
+
+// PRUEBAS PARA CONTAR LA CANTIDAD DE CARACTERES DEL ARCHIVO
+
+// std::vector<std::string> getFile(const std::string &file) {
+//     std::vector<std::string> file_;
+//     std::string line;
+//     std::ifstream inputFile(file);
+
+//     while (std::getline(inputFile, line)) {
+//         file_.push_back(line);
+//     }
+//     inputFile.close();
+//     return file_;
+// }
+
+// int countCharacters(const std::string& file_) {
+//     std::ifstream file(file_, std::ios::binary); 
+//     if (!file) {
+//         std::cerr << "Error abriendo el archivo\n";
+//         return -1;
+//     }
+
+//     int count = 0;
+//     char ch;
+//     while (file.get(ch)) { 
+//         count++;
+//     }
+    
+//     return count;
+// }
+
+
+// CODIGO REAL 
+
+// Lee el archivo como bytes y los almacena en un vector
+std::vector<unsigned char> getBytes(const std::string &file){
+    std::vector<unsigned char> bytes;
+    std::ifstream inputFile(file, std::ios::binary);
+
+    if(!inputFile){
+        std::cerr << "Error abriendo el archivo";
+        return bytes;
+    }
+
+    unsigned char byte;
+    while(inputFile.read(reinterpret_cast<char*>(&byte), sizeof(byte))){
+        bytes.push_back(byte);
+    }
+    inputFile.close();
+    return bytes;
+}
+
+// Ordena los bytes basado en su valor hexadecimal
+void sortBytes(std::vector<unsigned char>& bytes){
+    std::sort(bytes.begin(), bytes.end(), 
+    [](unsigned char &a, unsigned char &b){
+        return a < b;
+    });
+}
+
+// Pasa los bytes ordenados a un nuevo archivo, regresandolos a sus valores originales
+void generateFile(const std::vector<unsigned char>& bytes, const std::string& outputFile){
+    std::ofstream output(outputFile, std::ios::binary);
+
+    if (!output) {
+        std::cerr << "Error creando el archivo.\n";
+        return;
+    }
+
+    for (const auto &byte : bytes) {
+        output.write(reinterpret_cast<const char*>(&byte), sizeof(byte));
+    }
+    output.close();
+}
+
+int main() {
+    std::string file = "sorted_sample.txt";
+
+    // REVISION DE LA CANTIDAD DE CARACTERES
+
+    // std::vector<std::string> file_ = getFile(file);
+    // int size = countCharacters(file);
+
+    std::vector<unsigned char> bytes = getBytes(file);
+    sortBytes(bytes);
+    generateFile(bytes, "sorted_output.txt");
+    std::cout << "File sorted successfully. Check sorted_output.txt\n";
+    return 0;
+}
