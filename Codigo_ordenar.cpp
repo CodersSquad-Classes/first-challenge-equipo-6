db3b902a206c4b8140b80906df5aae8b35298f89
Upland1 <107065818+Upland1@users.noreply.github.com>
2025-02-15 19:37:40 -0600

    Update Codigo_ordenar.cpp

--- Codigo_ordenar.cpp
+++ Codigo_ordenar.cpp
@@ -86,7 +86,7 @@ int main() {
 
     std::vector<unsigned char> bytes = getBytes(file);
     sortBytes(bytes);
-    generateFile(bytes, "sorted_output.txt");
+    generateFile(bytes, "sorted_data.txt");
     std::cout << "Listo\n";
     return 0;
 }
